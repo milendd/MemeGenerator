4d4a010cb3b0f3c6659ca7cbba18f95449114b14
Milen Donchev <milen_d@abv.bg>
2018-06-10 12:53:04 +0300

    Code clean

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -5,16 +5,16 @@ abstract class BaseController {
     protected $actionName;
     protected $layoutName = DEFAULT_LAYOUT;
     protected $isViewRendered = false;
-	protected $isLoggedIn = false;
+    protected $isLoggedIn = false;
 
     function __construct($controllerName, $actionName) {
         $this->controllerName = $controllerName;
         $this->actionName = $actionName;
         $this->onInit();
-		
-		if (isset($_SESSION['user'])) {
-			$this->isLoggedIn = true;
-		}
+        
+        if (isset($_SESSION['user'])) {
+            $this->isLoggedIn = true;
+        }
     }
 
     public function onInit() {
@@ -30,19 +30,19 @@ abstract class BaseController {
             if ($viewName == null) {
                 $viewName = $this->actionName;
             }
-			
+            
             $viewFileName = "views/$this->controllerName/$viewName.php";
             if ($includeLayout) {
                 $headerFile = "views/layouts/$this->layoutName/header.php";
                 include_once($headerFile);
             }
-			
+            
             include_once($viewFileName);
             if ($includeLayout) {
                 $footerFile = "views/layouts/$this->layoutName/footer.php";
                 include_once($footerFile);
             }
-			
+            
             $this->isViewRendered = true;
         }
     }
@@ -57,16 +57,16 @@ abstract class BaseController {
         if ($actionName != null) {
             $url .= '/' . urlencode($actionName);
         }
-		
+        
         if ($params != null) {
             $encodedParams = array_map($params, 'urlencode');
             $url .= implode('/', $encodedParams);
         }
-		
+        
         $this->redirectToUrl($url);
     }
-	
-	protected function isPost() {
+    
+    protected function isPost() {
         return $_SERVER['REQUEST_METHOD'] == 'POST';
     }
 
@@ -74,16 +74,16 @@ abstract class BaseController {
         if (!isset($_SESSION['messages'])) {
             $_SESSION['messages'] = array();
         };
-		
+        
         array_push($_SESSION['messages'], array('text' => $msg, 'type' => $type));
     }
-	
-	function authorize($msg) {
-		if (!$this->isLoggedIn) {
-			$this->addErrorMessage($msg);
-			$this->redirect('account', 'login');
-		}
-	}
+    
+    function authorize($msg) {
+        if (!$this->isLoggedIn) {
+            $this->addErrorMessage($msg);
+            $this->redirect('account', 'login');
+        }
+    }
 
     function addInfoMessage($msg) {
         $this->addMessage($msg, 'info');
